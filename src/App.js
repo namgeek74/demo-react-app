896f4b150f1c11ca3638a03474fb51776d6b9173
namnd <dinhnambk96@gmail.com>
2021-10-12 21:17:05 +0700

    todo app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello ReactJS</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentTodo: '',
+      todoList: [],
+    }
+  }
+
+  changeTodo = (e) => {
+    this.setState({ currentTodo: e.target.value });
+  }
+
+  addTodo = () => {
+    this.setState({ todoList: [...this.state.todoList, this.state.currentTodo] });
+    this.setState({ currentTodo: '' });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Todo App</h1>
+        <label>Action: </label>
+        <input type="text" value={this.state.currentTodo} onChange={this.changeTodo} />
+        <button type="button" onClick={this.addTodo}>Add</button>
+        <ul>
+          {
+            this.state.todoList.map((item, index) => {
+              return (
+                <li key={index}>{item}</li>
+              );
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+
 }
 
 export default App;
